01bf2282bbe3930cb3768130a904b0f16470214a
Josh Donnell <josh.d@weareframework.co.uk>
2025-12-03 01:10:25 +0000

    fix: update files with changes

--- resources/ts/components/site/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <footer class="site-footer bg-white">
-    <div class="container">Footer to go here</div>
-  </footer>
-</template>
--- resources/ts/components/site/Header.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <header class="site-header bg-white">
-    <div class="container">Header to go here</div>
-  </header>
-</template>
